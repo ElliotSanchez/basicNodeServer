f2466f63836e6285f3fb4c0ac33a3f24455d3dce
Elliot Sanchez <github@elliotsanchez.com>
2017-03-21 15:51:49 -0500

    Request handlers added

---
+++ requestHandlers.js
@@ -0,0 +1,9 @@
+function start() {
+  console.log("reqHa.js : Request handler 'start' was called.");
+}
+function upload() {
+  console.log("reqHa.js : Request handler 'upload' was called.");
+}
+
+exports.start = start;
+exports.upload = upload;
